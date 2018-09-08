3e492096b91ef79bb4fbf0ad9eb7ad208c61a13e
Viral Patel <exc.viral@gmail.com>
2018-09-08 16:39:52 +0530

    Implemented simple normalization function
    Method of normalization: divide all the elements of dataset with maximum value to normalize data between 0 and 1.

---
+++ Feature extractors/normalization_functions.py
@@ -0,0 +1,26 @@
+# This script contains functions that will be helpful for normalizing dataset
+
+# There are numerous techniques available for normalizing the data in statistics, however I will be implementing
+# basic ones for now, later on I will implement other normalization techniques in this script.
+
+# Simplest normalization technique: Divide each value in the dataset with the maximum value in the dataset.
+# TODO: Other normalization technique: (X-mu)/sigma
+
+def simple_normalize(data):
+    '''
+    Simplest normalization function
+
+    This function takes in a list, finds the maximum element in the list and divides all the elements of the list by
+    maximum element, and returns the list with maximum element appended to the end of list. As a result, the data is
+    normalized between 0 and 1.
+
+    :param data: (list) containing features to be normalized
+    :return: (list) normalized features + maximum value
+    '''
+    max_elem = max(data)
+    normalized_data = [i / max_elem for i in data]
+    normalized_data.append(max_elem)
+    return normalized_data
+
+
+print(simple_normalize([5, 4, 3, 2, 1]))
